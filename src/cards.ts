d66c871b596567711a31f189fc45f58c8b78d233
S Kota <91359399+Skota11@users.noreply.github.com>
2024-02-25 22:14:10 +0900

    大幅改善

---
+++ src/cards.ts
@@ -0,0 +1,16 @@
+export const cards = [{num : 0 , suit : 0}, 
+    {num : 1 , suit : 1}, {num : 1 , suit : 2}, {num : 1 , suit : 3}, {num : 1 , suit : 4},
+    {num : 2 , suit : 1}, {num : 2 , suit : 2}, {num : 2 , suit : 3}, {num : 2 , suit : 4},
+    {num : 3 , suit : 1}, {num : 3 , suit : 2}, {num : 3 , suit : 3}, {num : 3 , suit : 4},
+    {num : 4 , suit : 1}, {num : 4 , suit : 2}, {num : 4 , suit : 3}, {num : 4 , suit : 4},
+    {num : 5 , suit : 1}, {num : 5 , suit : 2}, {num : 5 , suit : 3}, {num : 5 , suit : 4},
+    {num : 6 , suit : 1}, {num : 6 , suit : 2}, {num : 6 , suit : 3}, {num : 6 , suit : 4},
+    {num : 7 , suit : 1}, {num : 7 , suit : 2}, {num : 7 , suit : 3}, {num : 7 , suit : 4},
+    {num : 8 , suit : 1}, {num : 8 , suit : 2}, {num : 8 , suit : 3}, {num : 8 , suit : 4},
+    {num : 9 , suit : 1}, {num : 9 , suit : 2}, {num : 9 , suit : 3}, {num : 9 , suit : 4},
+    {num : 10 , suit : 1}, {num : 10 , suit : 2}, {num : 10 , suit : 3}, {num : 10 , suit : 4},
+    {num : 11 , suit : 1}, {num : 11 , suit : 2}, {num : 11 , suit : 3}, {num : 11 , suit : 4},
+    {num : 12 , suit : 1}, {num : 12 , suit : 2}, {num : 12 , suit : 3}, {num : 12 , suit : 4},
+    {num : 13 , suit : 1}, {num : 13 , suit : 2}, {num : 13 , suit : 3}, {num : 13 , suit : 4}]
+
+    
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { serve } from "https://deno.land/std/http/server.ts";
 import { Server } from "https://deno.land/x/socket_io/mod.ts";
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
+import { cards } from "./cards.ts";
+
 const app = new Application();
 
 const io = new Server({cors: {
@@ -12,12 +14,16 @@ const io = new Server({cors: {
 const data = new Map();
 const roomsInviteNumber = new Map();
 const roomsId = new Map();
+const startingRoom = new Map();
+const PlayerCards = new Map();
+const PlayerNames = new Map();
 
 io.on("connection", (socket) => {
     socket.on("createNewRoom", (msg) => {
         const id = crypto.randomUUID();
         io.to(socket.id).emit("NewRoomId", { id: id });
         data.set(id, msg)
+        startingRoom.set(id , false)
     })
     socket.on("joinRoom", (msg) => {
         if (socket.rooms.size == 2) {
@@ -28,6 +34,8 @@ io.on("connection", (socket) => {
         io.to(socketRoom).emit("Join", { username: msg.username, roomSettings: data.get(socketRoom), id: socketRoom })
         socket.data.username = msg.username;
         socket.data.room = socketRoom
+
+        PlayerNames.set(socket.id , msg.username);
     })
     socket.on("getInviteNumber", (msg) => {
         const socketRoom = Array.from(socket.rooms)[1]
@@ -51,11 +59,64 @@ io.on("connection", (socket) => {
             io.to(socket.id).emit("roomId", "invalidNumber")
         }
     })
-    socket.on("getInviteNumber", (msg) => {
-        const socketRoom = Array.from(socket.rooms)[1]
+    socket.on("getRoomMember" , (msg) => {
+        const members = io.of("/").adapter.rooms;
+        io.to(socket.id).emit("getRoomMember" , Array.from(members.get(msg)))
+    })
+    socket.on("gameStart", (msg) => {
+        console.log(io.of("/").adapter.rooms.get(msg).size)
+        if (!startingRoom.get(msg) && io.of("/").adapter.rooms.get(msg).size !== 1) {
+            const socketRoom = Array.from(socket.rooms)[1]
         if (msg == socketRoom) {
-            io.to(msg).emit("gameStart")
+            //gameStart
+            io.to(socketRoom).emit("gameStart" , {already : false , not : false})
+            startingRoom.set(msg , true)
+
+            const gameSetCards = [...cards];
+            const members = io.of("/").adapter.rooms.get(msg);
+            const array = Array.from(members)
+            const oneMemberCards = Math.floor(53 / members.size);
+            
+            for (let index = 0; index < array.length; index++) {
+                const playerId = array[index];
+                const temp:any = [];
+                for (let index = 0; index < oneMemberCards; index++) {
+                    const random = Math.floor(Math.random() * (gameSetCards.length))
+                    const element = gameSetCards[random];
+                    temp.push(element)
+                    gameSetCards.splice(random , 1)
+                }
+                PlayerCards.set(playerId , temp)
+            }
+            if (gameSetCards.length > 0) {
+                for (let index = 0; index < gameSetCards.length; index++) {
+                    let indexN = index;
+                    if (index > array.length) {
+                        indexN = index - array.length
+                    }
+                    const temp = Array.from(PlayerCards.get(array[indexN]))
+                    temp.push(gameSetCards[index])
+                    PlayerCards.set(array[indexN] , temp)
+                }
+            }
+
+            //手札をプレイヤーに送信
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+                console.log(element)
+                console.log(PlayerCards.get(element))
+                io.to(element).emit("getCards" , PlayerCards.get(element))
+            }
         }
+    }else if(io.of("/").adapter.rooms.get(msg).size == 1) {
+        io.to(socket.id).emit("gameStart" , {already : false , not : true})
+    }else {
+        io.to(socket.id).emit("gameStart" , {already : true , not : false})
+    }
+    })
+    socket.on("getPlayerName", (id) => {
+        io.to(socket.id).emit("getPlayerName" , Object.fromEntries(PlayerNames))
+        console.log(Object.fromEntries(PlayerNames))
     })
     socket.on("disconnect", () => {
         if (socket.data.room !== undefined) {
